0359477816adab529d25ce1efe57fae9dc6f4b88
shenguanpu <shenguanpu@360.cn>
2015-06-14 21:33:55 +0800

    first init

---
+++ ios/date-fitness/date-fitness/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  date-fitness
+//
+//  Created by Shen Guanpu on 15/6/14.
+//  Copyright (c) 2015年 Shen Guanpu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
